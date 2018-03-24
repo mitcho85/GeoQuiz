b8b4d588a1cbac6a79d82fc195ba80acbf7fed31
mitcho85 <mitcho85@gmail.com>
2018-03-24 11:46:13 -0700

    Added butterknife dependency.

--- app/src/main/java/com/software/digitals/geoquiz/QuizActivity.java
+++ app/src/main/java/com/software/digitals/geoquiz/QuizActivity.java
@@ -5,11 +5,35 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+
 public class QuizActivity extends AppCompatActivity {
-    public Button mTrueButton;
-    public Button mFalseButton;
+
+    @BindView(R.id.true_button)
+    private Button mTrueButton;
+
+    @BindView(R.id.false_button)
+    private Button mFalseButton;
+
+    @BindView(R.id.next_button)
+    private Button mNextButton;
+
+    @BindView(R.id.question_text_view)
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_austrialia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +41,6 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         // initialize references to buttons
-        mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,7 +51,6 @@ public class QuizActivity extends AppCompatActivity {
                 toast.show();
             }
         });
-        mFalseButton = findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
